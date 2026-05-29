269e9a2a016cf6f6ec11cf0a6eb8657696cea91b
Artur Spychalski <103207288+skullboypl@users.noreply.github.com>
2026-05-30 00:01:30 +0200

    feat: link do repozytorium GitHub w stopce z ikoną SVG

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,9 @@
 import { useContext } from 'react';
 import { LanguageContext } from '../generator/Generator.tsx';
+import { BrandGithubIcon } from '../assets/icons/tabler/BrandGithubIcon.tsx';
+
+const GITHUB_REPO_URL =
+  'https://github.com/skullboypl/faceit-banner-faceitbanner.vxh.pl';
 
 export const Footer = () => {
   const tl = useContext(LanguageContext);
@@ -9,6 +13,49 @@ export const Footer = () => {
   }
 
   return (
-    <footer className="app-footer"><div className="footer-left"><small>Ten projekt nie jest powiązany z <a href="https://faceit.com" target="_blank" rel="noreferrer">FACEIT</a>.</small><small>Ten projekt jest pochodną projektu z licencją: <a href="https://github.com/mxgic1337/faceit-stats-widget/blob/main/LICENSE" target="_blank" rel="noreferrer">MIT License</a></small><small><a href="/wiki/">Wiki</a></small></div><div className="footer-right"><small>Copyright © <a href="https://github.com/skullboypl" target="_blank" rel="noreferrer">Skull</a> {new Date().getFullYear()}</small></div></footer>
+    <footer className="app-footer">
+      <div className="footer-left">
+        <small>
+          Ten projekt nie jest powiązany z{' '}
+          <a href="https://faceit.com" target="_blank" rel="noreferrer">
+            FACEIT
+          </a>
+          .
+        </small>
+        <small>
+          Ten projekt jest pochodną projektu z licencją:{' '}
+          <a
+            href="https://github.com/mxgic1337/faceit-stats-widget/blob/main/LICENSE"
+            target="_blank"
+            rel="noreferrer"
+          >
+            MIT License
+          </a>
+        </small>
+        <small>
+          <a href="/wiki/">Wiki</a>
+        </small>
+      </div>
+      <div className="footer-right">
+        <a
+          className="footer-github-link"
+          href={GITHUB_REPO_URL}
+          target="_blank"
+          rel="noreferrer"
+          aria-label="Repozytorium na GitHubie"
+          title="Kod źródłowy na GitHubie"
+        >
+          <BrandGithubIcon />
+          <span>GitHub</span>
+        </a>
+        <small>
+          Copyright ©{' '}
+          <a href="https://github.com/skullboypl" target="_blank" rel="noreferrer">
+            Skull
+          </a>{' '}
+          {new Date().getFullYear()}
+        </small>
+      </div>
+    </footer>
   );
 };
